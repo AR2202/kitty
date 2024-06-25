535c68919551408de5945b0747c1f5ea7e13a492
AnnikaRings <annika.rings@optellum.com>
2024-06-25 20:45:33 +0100

    started adding tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE OverloadedStrings#-}
+import Test.Hspec
+import Test.QuickCheck
+import Parser
+import Evaluator
+import TypeChecker
+import KittyTypes
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+    do
+      -- Parsing
+      -----------------------------------------
+      parseAddition
+
+
+parseAdd :: Expectation
+parseAdd =
+  parseAsAST  "1 + 2"
+    `shouldBe` Right (Expr Add (IntLit 1) (IntLit 2))
+
+parseAddition :: SpecWith ()
+parseAddition =
+  describe "parseAsAST" $
+    context "when parsing an addition expression" $
+      it
+        "should return Expr Add (lit1) (lit2)"
+        parseAdd
