ee3637755127ddd4c361462023d603e39ec8cf70
AnnikaRings <annika.rings@optellum.com>
2024-06-25 21:54:24 +0100

    added tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,21 +1,24 @@
-{-# LANGUAGE OverloadedStrings#-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Evaluator
+import KittyTypes
+import Parser
 import Test.Hspec
 import Test.QuickCheck
-import Parser
-import Evaluator
 import TypeChecker
-import KittyTypes
+
 main :: IO ()
 main = hspec $
-    do
-      -- Parsing
-      -----------------------------------------
-      parseAddition
-
+  do
+    -- Parsing
+    -----------------------------------------
+    parseAddition
+    parseParens
+    parseParensTimes
 
 parseAdd :: Expectation
 parseAdd =
-  parseAsAST  "1 + 2"
+  parseAsAST "1 + 2"
     `shouldBe` Right (Expr Add (IntLit 1) (IntLit 2))
 
 parseAddition :: SpecWith ()
@@ -25,3 +28,28 @@ parseAddition =
       it
         "should return Expr Add (lit1) (lit2)"
         parseAdd
+
+parseParensChangeOrder :: Expectation
+parseParensChangeOrder =
+  parseAsAST " 1 + (2 - 1)"
+    `shouldBe` Right (Expr Add (IntLit 1) (Parens (Expr Sub (IntLit 2) (IntLit 1))))
+
+parseParensBeforeTimes :: Expectation
+parseParensBeforeTimes =
+  parseAsAST " 1 * (2 - 1)"
+    `shouldBe` Right (Expr Mult (IntLit 1) (Parens (Expr Sub (IntLit 2) (IntLit 1))))
+
+parseParens :: SpecWith ()
+parseParens =
+  describe "parseAsAST" $
+    context "when parsing parentheses" $
+      it
+        "should parse parenthesised expression with higher precenence"
+        parseParensChangeOrder
+parseParensTimes :: SpecWith ()
+parseParensTimes =
+  describe "parseAsAST" $
+    context "when parsing parentheses" $
+      it
+        "should parse parenthesised expression with higher precenence"
+        parseParensBeforeTimes
